6ac73b02fd69ff05558b3457ba33421f236c3e26
Mrinal kanti sardar <mrinalkantisardar007@gmail.com>
2023-06-18 18:14:59 +0530

    server setup #2

--- app.js
+++ app.js
@@ -2,7 +2,23 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Server Running");
+  res.send("This is the Home Route");
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is the about Route");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("This is the contact Route");
+});
+
+app.get("/signin", (req, res) => {
+  res.send("This is the signin Route");
+});
+
+app.get("/signup", (req, res) => {
+  res.send("This is the signup Route");
 });
 
 app.listen(3000, () => {
